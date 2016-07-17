8f949cb104859d6c239a363f7ca28dcab6bfacd1
Alex Hirsch <w4rh4wk@bluephoenix.at>
2016-07-17 11:15:30 +0200

    Front: Improve lexer whitespace handling and position tracking

--- src/front/manual_parser/lexer.rs
+++ src/front/manual_parser/lexer.rs
@@ -1,5 +1,7 @@
 use std::iter::Peekable;
 
+use front::Position;
+
 #[derive(PartialEq, Debug)]
 pub enum TokenType {
     NOT,
@@ -45,29 +47,60 @@ pub enum TokenType {
     Str(String),
 }
 
+#[derive(PartialEq, Debug)]
+pub struct Token {
+    tok: TokenType,
+    pos: Position,
+}
+
 pub struct Lexer<I: Iterator<Item = char>> {
     input: Peekable<I>,
-    line: i32,
-    col: i32,
+    pos: Position,
 }
 
 impl<I: Iterator<Item = char>> Lexer<I> {
     fn new(input: I) -> Lexer<I> {
         Lexer {
             input: input.peekable(),
-            line: 0,
-            col: 0,
+            pos: Position { line: 1, col: 1 },
+        }
+    }
+
+    fn get_char(&mut self) -> Option<char> {
+        self.input.next().map(|c| {
+            self.update_position(c);
+            c
+        })
+    }
+
+    fn get_char_if<P>(&mut self, predicate: P) -> Option<char>
+        where P: Fn(&char) -> bool
+    {
+        if self.input.peek().map(predicate).unwrap_or(false) {
+            self.get_char()
+        } else {
+            None
+        }
+    }
+
+    fn update_position(&mut self, c: char) {
+        match c {
+            '\n' => {
+                self.pos.line += 1;
+                self.pos.col = 1;
+            }
+            '\t' => self.pos.col += 7,
+            _ => self.pos.col += 1,
         }
     }
 
     fn lex_word(&mut self, c: char) -> TokenType {
-        let mut ident = self.input
-            .by_ref()
-            .take_while(|&c| c.is_alphabetic() || c.is_numeric() || c == '_')
-            .collect::<String>();
-        ident.insert(0, c);
+        let mut s = c.to_string();
+        while let Some(c) = self.get_char_if(|&c| c.is_alphabetic() || c.is_numeric() || c == '_') {
+            s.push(c);
+        }
 
-        match ident.as_ref() {
+        match s.as_ref() {
             // handle type keywords
             "void" => TokenType::KwVoid,
             "bool" => TokenType::KwBool,
@@ -85,16 +118,15 @@ impl<I: Iterator<Item = char>> Lexer<I> {
             "return" => TokenType::KwReturn,
 
             // handle identifier
-            _ => TokenType::Identifier(ident),
+            _ => TokenType::Identifier(s),
         }
     }
 
     fn lex_number(&mut self, c: char) -> TokenType {
-        let mut num = self.input
-            .by_ref()
-            .take_while(|&c| c.is_numeric() || c == '.')
-            .collect::<String>();
-        num.insert(0, c);
+        let mut num = c.to_string();
+        while let Some(n) = self.get_char_if(|&c| c.is_numeric() || c == '.') {
+            num.push(n);
+        }
 
         if let Ok(n) = num.parse::<i32>() {
             TokenType::INum(n)
@@ -106,22 +138,33 @@ impl<I: Iterator<Item = char>> Lexer<I> {
     }
 
     fn lex_string(&mut self) -> TokenType {
-        TokenType::Str(self.input.by_ref().take_while(|&c| c != '\"').collect())
+        let mut s = "".to_string();
+        while let Some(c) = self.get_char_if(|&c| c != '\"') {
+            s.push(c);
+        }
+
+        // pop off \"
+        if self.input.peek().is_some() {
+            self.get_char();
+        }
+
+        TokenType::Str(s)
     }
 }
 
 impl<I: Iterator<Item = char>> Iterator for Lexer<I> {
-    type Item = TokenType;
+    type Item = Token;
 
-    fn next(&mut self) -> Option<TokenType> {
+    fn next(&mut self) -> Option<Token> {
         loop {
             // handle EOF
             if self.input.peek().is_none() {
                 return None;
             }
 
-            return Some(match self.input.next().unwrap() {
-                // handle WS
+            let pos = self.pos;
+
+            let tok = match self.get_char().unwrap() {
                 ' ' | '\t' | '\n' | '\r' => continue,
 
                 // handle Syntax
@@ -143,16 +186,14 @@ impl<I: Iterator<Item = char>> Iterator for Lexer<I> {
 
                 // handle Ops (complex)
                 '=' => {
-                    if let Some(&'=') = self.input.peek() {
-                        self.input.next();
+                    if self.get_char_if(|&c| c == '=').is_some() {
                         TokenType::EQ
                     } else {
                         TokenType::ASSIGN
                     }
                 }
                 '<' => {
-                    if let Some(&'=') = self.input.peek() {
-                        self.input.next();
+                    if self.get_char_if(|&c| c == '=').is_some() {
                         TokenType::LE
                     } else {
                         TokenType::LT
@@ -169,6 +210,11 @@ impl<I: Iterator<Item = char>> Iterator for Lexer<I> {
                 '\"' => self.lex_string(),
 
                 x @ _ => panic!("unexpected input: `{}`", x),
+            };
+
+            return Some(Token {
+                tok: tok,
+                pos: pos,
             });
         }
     }
@@ -180,65 +226,100 @@ mod test {
 
     #[test]
     fn syntax() {
-        let mut l = Lexer::new("! == < <= = + - * / , ; ( [ { } ] )".chars());
-        assert_eq!(l.next(), Some(TokenType::NOT));
-        assert_eq!(l.next(), Some(TokenType::EQ));
-        assert_eq!(l.next(), Some(TokenType::LT));
-        assert_eq!(l.next(), Some(TokenType::LE));
-        assert_eq!(l.next(), Some(TokenType::ASSIGN));
-        assert_eq!(l.next(), Some(TokenType::PLUS));
-        assert_eq!(l.next(), Some(TokenType::MINUS));
-        assert_eq!(l.next(), Some(TokenType::ASTER));
-        assert_eq!(l.next(), Some(TokenType::SLASH));
-        assert_eq!(l.next(), Some(TokenType::Comma));
-        assert_eq!(l.next(), Some(TokenType::SemiColon));
-        assert_eq!(l.next(), Some(TokenType::LParenth));
-        assert_eq!(l.next(), Some(TokenType::LBracket));
-        assert_eq!(l.next(), Some(TokenType::LBrace));
-        assert_eq!(l.next(), Some(TokenType::RBrace));
-        assert_eq!(l.next(), Some(TokenType::RBracket));
-        assert_eq!(l.next(), Some(TokenType::RParenth));
-        assert_eq!(l.next(), None);
+        let mut l = Lexer::new("! == < <= = + - * / , ; ([{}])".chars());
+        assert_eq!(TokenType::NOT, l.next().unwrap().tok);
+        assert_eq!(TokenType::EQ, l.next().unwrap().tok);
+        assert_eq!(TokenType::LT, l.next().unwrap().tok);
+        assert_eq!(TokenType::LE, l.next().unwrap().tok);
+        assert_eq!(TokenType::ASSIGN, l.next().unwrap().tok);
+        assert_eq!(TokenType::PLUS, l.next().unwrap().tok);
+        assert_eq!(TokenType::MINUS, l.next().unwrap().tok);
+        assert_eq!(TokenType::ASTER, l.next().unwrap().tok);
+        assert_eq!(TokenType::SLASH, l.next().unwrap().tok);
+        assert_eq!(TokenType::Comma, l.next().unwrap().tok);
+        assert_eq!(TokenType::SemiColon, l.next().unwrap().tok);
+        assert_eq!(TokenType::LParenth, l.next().unwrap().tok);
+        assert_eq!(TokenType::LBracket, l.next().unwrap().tok);
+        assert_eq!(TokenType::LBrace, l.next().unwrap().tok);
+        assert_eq!(TokenType::RBrace, l.next().unwrap().tok);
+        assert_eq!(TokenType::RBracket, l.next().unwrap().tok);
+        assert_eq!(TokenType::RParenth, l.next().unwrap().tok);
+        assert_eq!(None, l.next());
     }
 
     #[test]
     fn keywords() {
-        let mut l = Lexer::new("void bool int float true false if else while return".chars());
-        assert_eq!(l.next(), Some(TokenType::KwVoid));
-        assert_eq!(l.next(), Some(TokenType::KwBool));
-        assert_eq!(l.next(), Some(TokenType::KwInt));
-        assert_eq!(l.next(), Some(TokenType::KwFloat));
-        assert_eq!(l.next(), Some(TokenType::KwTrue));
-        assert_eq!(l.next(), Some(TokenType::KwFalse));
-        assert_eq!(l.next(), Some(TokenType::KwIf));
-        assert_eq!(l.next(), Some(TokenType::KwElse));
-        assert_eq!(l.next(), Some(TokenType::KwWhile));
-        assert_eq!(l.next(), Some(TokenType::KwReturn));
-        assert_eq!(l.next(), None);
+        let mut l = Lexer::new("void bool int float true false if else while,return".chars());
+        assert_eq!(TokenType::KwVoid, l.next().unwrap().tok);
+        assert_eq!(TokenType::KwBool, l.next().unwrap().tok);
+        assert_eq!(TokenType::KwInt, l.next().unwrap().tok);
+        assert_eq!(TokenType::KwFloat, l.next().unwrap().tok);
+        assert_eq!(TokenType::KwTrue, l.next().unwrap().tok);
+        assert_eq!(TokenType::KwFalse, l.next().unwrap().tok);
+        assert_eq!(TokenType::KwIf, l.next().unwrap().tok);
+        assert_eq!(TokenType::KwElse, l.next().unwrap().tok);
+        assert_eq!(TokenType::KwWhile, l.next().unwrap().tok);
+        assert_eq!(TokenType::Comma, l.next().unwrap().tok);
+        assert_eq!(TokenType::KwReturn, l.next().unwrap().tok);
+        assert_eq!(None, l.next());
     }
 
     #[test]
     fn identifiers() {
         let mut l = Lexer::new("foo b4r ba_".chars());
-        assert_eq!(l.next(), Some(TokenType::Identifier("foo".to_string())));
-        assert_eq!(l.next(), Some(TokenType::Identifier("b4r".to_string())));
-        assert_eq!(l.next(), Some(TokenType::Identifier("ba_".to_string())));
-        assert_eq!(l.next(), None);
+        assert_eq!(TokenType::Identifier("foo".to_string()),
+                   l.next().unwrap().tok);
+        assert_eq!(TokenType::Identifier("b4r".to_string()),
+                   l.next().unwrap().tok);
+        assert_eq!(TokenType::Identifier("ba_".to_string()),
+                   l.next().unwrap().tok);
+        assert_eq!(None, l.next());
     }
 
     #[test]
     fn numbers() {
         let mut l = Lexer::new("42 3.14".chars());
-        assert_eq!(l.next(), Some(TokenType::INum(42)));
-        assert_eq!(l.next(), Some(TokenType::FNum(3.14)));
-        assert_eq!(l.next(), None);
+        assert_eq!(TokenType::INum(42), l.next().unwrap().tok);
+        assert_eq!(TokenType::FNum(3.14), l.next().unwrap().tok);
+        assert_eq!(None, l.next());
     }
 
     #[test]
     fn strings() {
         let mut l = Lexer::new("\"foo\" \"bar\"".chars());
-        assert_eq!(l.next(), Some(TokenType::Str("foo".to_string())));
-        assert_eq!(l.next(), Some(TokenType::Str("bar".to_string())));
-        assert_eq!(l.next(), None);
+        assert_eq!(TokenType::Str("foo".to_string()), l.next().unwrap().tok);
+        assert_eq!(TokenType::Str("bar".to_string()), l.next().unwrap().tok);
+        assert_eq!(None, l.next());
+    }
+
+    #[test]
+    fn position() {
+        // operator
+        let mut l = Lexer::new("+ == -".chars());
+        assert_eq!(1, l.next().unwrap().pos.col);
+        assert_eq!(3, l.next().unwrap().pos.col);
+        assert_eq!(6, l.next().unwrap().pos.col);
+
+        // number
+        l = Lexer::new("42 3.14".chars());
+        assert_eq!(1, l.next().unwrap().pos.col);
+        assert_eq!(4, l.next().unwrap().pos.col);
+
+        // keyword
+        l = Lexer::new("if while true".chars());
+        assert_eq!(1, l.next().unwrap().pos.col);
+        assert_eq!(4, l.next().unwrap().pos.col);
+        assert_eq!(10, l.next().unwrap().pos.col);
+
+        // strings
+        l = Lexer::new("\"foo\" \"bar\"".chars());
+        assert_eq!(1, l.next().unwrap().pos.col);
+        assert_eq!(7, l.next().unwrap().pos.col);
+
+        // lines
+        l = Lexer::new("foo\n\"bar\nbaz\"\nbas".chars());
+        assert_eq!((1, 1), l.next().unwrap().pos.to_pair());
+        assert_eq!((2, 1), l.next().unwrap().pos.to_pair());
+        assert_eq!((4, 1), l.next().unwrap().pos.to_pair());
     }
 }
