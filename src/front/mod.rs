99793565d3d085a6b1f4bee84f14a510a905602a
Alex Hirsch <w4rh4wk@bluephoenix.at>
2016-07-23 10:55:44 +0200

    Front: Add syntax specs for .gib files

--- src/front/mod.rs
+++ src/front/mod.rs
@@ -1,3 +1,64 @@
+//! # The `.gib` Syntax
+//!
+//! Input files to Cleaver follow the grammar described here and are called gibs (singular: gib).
+//! Expression written between slashes (`/`) are regular expressions (Javascript). Tokens are
+//! written in double-quotes (`"`) and rules between angular brackets (`<`, `>`). Something between
+//! brackets (`[`, `]`) is considered optional while something between braces (`{`, `}`) can occur
+//! multiple times or not at all.
+//!
+//! ```text
+//!     <identifier>        ::= /[A-Za-z][A-Za-z0-9_]*/
+//!
+//!     <bool_literal>      ::= "true" | "false"
+//!
+//!     <int_literal>       ::= /[0-9]+/
+//!
+//!     <float_literal>     ::= /[0-9]+\.[0-9]+/
+//!
+//!     <string_literal>    ::= /"[^"]"/
+//!
+//!     <type>              ::= "void" | "bool" | "int" | "float" | "string"
+//!
+//!     <un_op>             ::= "!" | "-"
+//!
+//!     <bin_op>            ::= "==" | "<" | "<=" | "+" | "-" | "*" | "/"
+//!
+//!     <variable>          ::= <identifier>
+//!
+//!     <expression>        ::= <bool_literal>
+//!                           | <int_literal>
+//!                           | <float_literal>
+//!                           | <string_literal>
+//!                           | <variable>
+//!                           | "(" <expression> ")"
+//!                           | <call_expression>
+//!                           | <un_op> <expression>
+//!                           | <expression> <bin_op> <expression>
+//!
+//!     <call_expression>   ::= <identifier> "(" [ <expression> { "," <expression> } ] ")"
+//!
+//!     <statement>         ::= <expression> ";"
+//!                           | <declaration> ";"
+//!                           | <assignment> ";"
+//!                           | <if_statement>
+//!                           | <while_statement>
+//!                           | <return> ";"
+//!                           | "{" { <statement } "}"
+//!
+//!     <declaration>       ::= <type> <variable>
+//!
+//!     <assignment>        ::= <variable> "=" <expression>
+//!
+//!     <if_statement>      ::= "if" "(" <expression> ")" <statement> [ "else" <statement> ]
+//!
+//!     <while_statement>   ::= "while" "(" <expression> ")" <statement>
+//!
+//!     <program>           ::= { <function> }
+//!
+//!     <function>          ::= <type> <identifier> "("
+//!                                 [ <declaration> { "," <declaration> } ] ")" <statement>
+//! ```
+
 pub mod ast;
 pub mod rd_parser;
 
