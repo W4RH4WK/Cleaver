0c7d52f42399b03e8f25eb7fc224e78186795bf4
Alex Hirsch <w4rh4wk@bluephoenix.at>
2016-08-02 21:25:11 +0200

    Remove obsolete test

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,3 @@ pub mod front;
 pub fn my_name() -> &'static str {
     "Cleaver"
 }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_name() {
-        assert_eq!("Cleaver", my_name());
-    }
-}
