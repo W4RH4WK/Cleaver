472c8846abe7fa71b0933e757e5d1f3db2c7c26f
Alex Hirsch <w4rh4wk@bluephoenix.at>
2016-08-21 22:08:26 +0200

    Front: Add CallsUnknownFunction error

--- src/analysis/semantic/calls.rs
+++ src/analysis/semantic/calls.rs
@@ -1,11 +1,51 @@
+use std::error::Error;
+use std::fmt;
+use std::result;
+
 use ::front::ast;
 
-pub fn check_target(functions: &ast::Functions, fun: &ast::Node<ast::Function>) {
+pub type Result<'a> = result::Result<(), CallsUnknownFunction<'a>>;
+
+pub fn check_target<'a>(functions: &'a ast::Functions,
+                        fun: &'a ast::Node<ast::Function>)
+                        -> Result<'a> {
+    let mut res = Ok(());
+
     fun.node.body.visit_expr(&mut |expr| {
         match expr.node {
-            ast::Expression::Call { ref function, .. } => assert!(functions.contains_key(function)),
-            _ => (),
-        };
-        true
+            ast::Expression::Call { ref function, .. } if !functions.contains_key(function) => {
+                res = Err(CallsUnknownFunction { call: expr });
+                false
+            }
+            _ => true,
+        }
     });
+
+    res
+}
+
+#[derive(PartialEq, Debug)]
+pub struct CallsUnknownFunction<'a> {
+    call: &'a ast::Node<ast::Expression>,
+}
+
+impl<'a> fmt::Display for CallsUnknownFunction<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.call.node {
+            ast::Expression::Call { ref function, .. } => {
+                write!(f,
+                       "{}.{} CallsUnknownFunction: `{}`",
+                       self.call.pos.line,
+                       self.call.pos.col,
+                       function)
+            }
+            _ => panic!("expression not an Expression::Call"),
+        }
+    }
+}
+
+impl<'a> Error for CallsUnknownFunction<'a> {
+    fn description(&self) -> &str {
+        "call to unknown function"
+    }
 }
